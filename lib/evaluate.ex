0810b0c68f3ffbfbf919dcf10bcea2951baeded1
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2020-02-08 21:46:36 -0500

    Handles multiple argument statements

--- lib/evaluate.ex
+++ lib/evaluate.ex
@@ -1,16 +1,42 @@
 defmodule FactEngine.Evaluate do
+  @type command() :: :input | :query
+  @type statement() :: String.t()
+  @type args() :: [String.t()]
+  @type output() :: [String.t()]
+
+  @type state() :: %{statement() => args | [args]}
+  # state = %{"is_a_cat": ["lucy"], "are_friends": [["lucy", "mike"]]}
+
+  @spec eval([{command(), statement(), args()}]) :: {:ok, output()}
   def eval(input) do
     {state, output} = Enum.reduce_while(input, {%{}, []}, &eval_command/2)
     {:ok, output}
   end
 
+  # @spec eval_command({command(), statement(), args()}) :: {:cont | :halt, state(), output()}
   defp eval_command({:input, statement, [arg]}, {state, output}) do
     {:cont, {Map.update(state, statement, [arg], fn old -> [arg | old] end), output}}
+  end
+
+  defp eval_command({:input, statement, args}, {state, output}) do
+    {:cont, {Map.update(state, statement, [args], fn old -> old ++ args end), output}}
   end
 
   defp eval_command({:query, statement, [arg]}, {state, output}) do
     if Map.has_key?(state, statement) do
       result = Enum.member?(state[statement], arg) |> to_string()
+      {:cont, {state, [result | output]}}
+    else
+      {:halt, {state, ["error: #{statement} is undefined"]}}
+    end
+  end
+
+  defp eval_command({:query, statement, args}, {state, output}) do
+    if Map.has_key?(state, statement) do
+      result =
+        Enum.any?(state[statement], fn x -> x == args || x == Enum.reverse(args) end)
+        |> to_string()
+
       {:cont, {state, [result | output]}}
     else
       {:halt, {state, ["error: #{statement} is undefined"]}}
--- test/evaluate_test.exs
+++ test/evaluate_test.exs
@@ -4,17 +4,26 @@ defmodule EvaluateTest do
 
   test "returns single argument statement as true when true" do
     input = [{:input, "is_a_cat", ["lucy"]}, {:query, "is_a_cat", ["lucy"]}]
-    assert {:ok, ["true"]} = Evaluate.eval(input)
+    assert {:ok, ["true"]} == Evaluate.eval(input)
   end
 
   test "returns single argument statement as false when false" do
     input = [{:input, "is_a_cat", ["lucy"]}, {:query, "is_a_cat", ["ben"]}]
-    assert {:ok, ["false"]} = Evaluate.eval(input)
+    assert {:ok, ["false"]} == Evaluate.eval(input)
   end
 
   test "returns error when statement is undefined" do
     input = [{:query, "is_a_cat", ["lucy"]}]
     # TODO give better error message
-    assert {:ok, ["error: is_a_cat is undefined"]} = Evaluate.eval(input)
+    assert {:ok, ["error: is_a_cat is undefined"]} == Evaluate.eval(input)
+  end
+
+  test "can handle multiple arg statements" do
+    input = [
+      {:input, "are_friends", ["alex", "sam"]},
+      {:query, "are_friends", ["alex", "sam"]}
+    ]
+
+    assert {:ok, ["true"]} == Evaluate.eval(input)
   end
 end
